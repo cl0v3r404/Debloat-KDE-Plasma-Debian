6b3f8e95b2bfd090ef38d9778c5c02a184f6aa4a
cl0v3r <47293977+cl0v3r404@users.noreply.github.com>
2025-06-12 14:38:53 +0200

    Update kdedebloat.sh
    Added applications of Live DVD installation

--- kdedebloat.sh
+++ kdedebloat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Aplicaciones 
+# Aplicaciones a eliminar
 APLICACIONES_INNECESARIAS=(
     "konqueror"  
     "kmail"    
@@ -10,8 +10,36 @@ APLICACIONES_INNECESARIAS=(
     "libkf5contacts-data"          
     "pim-sieve-editor"           
     "libkf5ksieve-data"          
-    "xterm"  
-    "zutty"        
+    "xterm"
+    "zutty"
+    "debian-reference-common"
+    "kasumi"
+    "anthy-common"
+    "goldendict"
+    "ibus-data"
+    "ibus-gtk"
+    "ibus-gtk3"
+    "ibus-gtk4"
+    "python3-ibus"
+    "ibus-hangul"
+    "gir1.2-ibus-1.0"
+    "mozc-data"
+    "mozc-server"
+    "fcitx5-modules"
+    "fcitx5-pinyin"
+    "fcitx5-modules-chttrans"
+    "libfcitx5-qt1"
+    "fcitx5-module-punctuation"
+    "fcitx5-data"
+    "libfcitx5-qt-data"
+    "fcitx5-chewing"
+    "fcitx5-chinese-addons-data"
+    "im-config"
+    "fcitx-frontend-qt5"
+    "fcitx-frontend-gtk3"
+    "fctix-frontend-gtk2"
+    "mlterm-common"
+    "xiterm+thai"
 )
 
 # Paquetes importantes
@@ -22,7 +50,7 @@ PAQUETES_IMPORTANTES=(
     "task-kde-desktop"         
 )
 
-# Función para marcar paquetes como instalados manualmente
+# Marcar paquetes como instalados manualmente
 marcar_como_manual() {
     for pkg in "${PAQUETES_IMPORTANTES[@]}"; do
         echo "Marcando $pkg como instalado manualmente..."
@@ -30,7 +58,7 @@ marcar_como_manual() {
     done
 }
 
-# Función para eliminar aplicaciones
+# Eliminar aplicaciones
 eliminar_aplicaciones() {
     for app in "${APLICACIONES_INNECESARIAS[@]}"; do
         echo "Eliminando $app..."
@@ -48,4 +76,4 @@ marcar_como_manual
 # Ejecutar la función de eliminación
 eliminar_aplicaciones
 
-echo "Proceso de eliminación completado."
+echo "Proceso de eliminación completado, se recomienda reiniciar el sistema."
