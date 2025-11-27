9470fbb06e9e4d02ad50b5a2ab7e471503d8c180
cl0v3r <47293977+cl0v3r404@users.noreply.github.com>
2025-11-27 12:32:39 +0100

    Update kdedebloat.sh
    Cambio en la estructura del script, añadidos nuevos paquetes para eliminar y añadidos paquetes a instalar despues del desinflado

--- kdedebloat.sh
+++ kdedebloat.sh
@@ -1,79 +1,18 @@
 #!/bin/bash
+set -e
 
-# Aplicaciones a eliminar
-APLICACIONES_INNECESARIAS=(
-    "konqueror"  
-    "kmail"    
-    "kwrite"    
-    "kmailtransport-akonadi"
-    "libkf5templateparser5"    
-    "libkf5contacts-data"          
-    "pim-sieve-editor"           
-    "libkf5ksieve-data"          
-    "xterm"
-    "zutty"
-    "debian-reference-common"
-    "kasumi"
-    "anthy-common"
-    "goldendict"
-    "ibus-data"
-    "ibus-gtk"
-    "ibus-gtk3"
-    "ibus-gtk4"
-    "python3-ibus"
-    "ibus-hangul"
-    "gir1.2-ibus-1.0"
-    "mozc-data"
-    "mozc-server"
-    "fcitx5-modules"
-    "fcitx5-pinyin"
-    "fcitx5-modules-chttrans"
-    "libfcitx5-qt1"
-    "fcitx5-module-punctuation"
-    "fcitx5-data"
-    "libfcitx5-qt-data"
-    "fcitx5-chewing"
-    "fcitx5-chinese-addons-data"
-    "im-config"
-    "fcitx-frontend-qt5"
-    "fcitx-frontend-gtk3"
-    "fctix-frontend-gtk2"
-    "mlterm-common"
-    "xiterm+thai"
-)
+echo "→ Comenzando desinflado de KDE Plasma..."
 
-# Paquetes importantes
-PAQUETES_IMPORTANTES=(
-    "kde-baseapps"  
-    "kde-plasma-desktop"      
-    "kde-standard"            
-    "task-kde-desktop"         
-)
+echo "→ Marcando para no desistalar suite de accesibilidad de KDE Plasma..."
+sudo apt-mark hold kdeaccessibility
 
-# Marcar paquetes como instalados manualmente
-marcar_como_manual() {
-    for pkg in "${PAQUETES_IMPORTANTES[@]}"; do
-        echo "Marcando $pkg como instalado manualmente..."
-        sudo apt install -y "$pkg"
-    done
-}
+echo "→ Desinstalando aplicaciones..."
+sudo apt remove --purge konqueror konq-plugins akregator kmail kaddressbook gimp xterm kwrite debian-reference-common dragonplayer juk goldendict-ng anthy anthy-common mozc-data uim-mozc mozc-server xiterm+thai fcitx-config-common fcitx-frontend-all fcitx5 fcitx-modules fcitx-module-quickphrase-editor fcitx5-data fcitx5-config ibus-data
 
-# Eliminar aplicaciones
-eliminar_aplicaciones() {
-    for app in "${APLICACIONES_INNECESARIAS[@]}"; do
-        echo "Eliminando $app..."
-        sudo apt-get remove --purge -y "$app"
-    done
+echo "→ Instalando herramientas varias..."
+sudo apt install plasma-discover-backend-flatpak kde-config-plymouth kde-config-flatpak
 
-    # Limpiar paquetes no necesarios después de la eliminación
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean -y
-}
+echo "→ Limpiando paquetes innecesarios..."
+sudo apt autoremove -y
 
-# Marcar paquetes importantes como manuales
-marcar_como_manual
-
-# Ejecutar la función de eliminación
-eliminar_aplicaciones
-
-echo "Proceso de eliminación completado, se recomienda reiniciar el sistema."
+echo "Post-instalación completada correctamente, se recomienda reiniciar."
